705c9a6532fb680de66ddeeda76a7f9e828a7168
PushwooshCI <help@pushwoosh.com>
2026-07-20 13:33:09 +0000

    ""

---
+++ src/modules/PlatformChecker/PlatformChecker.types.ts
@@ -0,0 +1,7 @@
+export type TPlatformSafari = 10; // Safari platform code in Pushwoosh system
+export type TPlatformChrome = 11; // Chrome platform code in Pushwoosh system
+export type TPlatformFirefox = 12; // Firefox platform code in Pushwoosh system
+export type TPlatformEdge = 150; // Edge platform code in Pushwoosh system
+export type TPlatform = TPlatformSafari | TPlatformChrome | TPlatformFirefox;
+
+export type TFormFactor = 'MOBILE' | 'DESKTOP';
